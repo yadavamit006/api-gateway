0d58e7468a4c57452549aa66f9f4064b9644ce0e
Amit Yadav <yadavamit006@gmail.com>
2024-12-04 12:02:18 +0530

    Changes

--- src/main/java/com/sample/microservices/apigateway/confg/ApiGatewayConfiguration.java
+++ src/main/java/com/sample/microservices/apigateway/confg/ApiGatewayConfiguration.java
@@ -7,18 +7,19 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ApiGatewayConfiguration {
-	
-	@Bean
-	public RouteLocator gatwayRouter(RouteLocatorBuilder locatorBuilder) {
-		return locatorBuilder
-				.routes()
-				.route(
-						p->p.path("/get")
-						.filters(f->f.addRequestHeader("MyHeader", "MyURI").addRequestHeader("Param", "MyValue"))
-						.uri("http://httpbin.org:80"))
-				.route(p->p.path("/currency-exchange/**").uri("lb://currency-exchange"))
-				.route(p->p.path("/currency-conversion/**","/currency-conversion-feign/**").uri("lb://currency-conversion-service"))
-				.build();
-	}
 
+    @Bean
+    public RouteLocator gatwayRouter(RouteLocatorBuilder locatorBuilder) {
+        return locatorBuilder.routes()
+                .route(p -> p.path("/get")
+                        .filters(f -> f.addRequestHeader("MyHeader", "MyURI")
+                                .addRequestHeader("Param", "MyValue"))
+                        .uri("http://httpbin.org:80"))
+
+                .route(p -> p.path("/currency-exchange/**")
+                        .uri("lb://currency-exchange"))
+
+                .route(p -> p.path("/currency-conversion/**", "/currency-conversion-feign/**")
+                        .uri("lb://currency-conversion-service")).build();
+    }
 }
